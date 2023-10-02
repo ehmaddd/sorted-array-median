04c10fd95be0a10b38220b60b5a762dfcb13fb29
ehmaddd <ehmaddd@gmail.com>
2023-10-02 13:02:58 +0500

    Adjust medium point

--- Solution.js
+++ Solution.js
@@ -3,15 +3,19 @@ var findMedianSortedArrays = function(nums1, nums2) {
   const merged = nums1.concat(nums2);
   const sorted = merged.sort(function(a, b){return a - b});
   if(sorted.length%2 == 0){
-    console.log('even');
+    const half = sorted.length / 2;
+    const halfplus = half+1;
+    const sum = sorted[half - 1] +sorted[halfplus - 1];
+    console.log(sum);
   }
   else {
-    console.log('odd');
+    const half = parseInt((sorted.length-1) / 2);
+    console.log(sorted[half]);
   }
 };
 
-const nums1 = [1,3];
-const nums2 = [2];
+const nums1 = [1, 2];
+const nums2 = [3, 4];
 findMedianSortedArrays(nums1, nums2);
 
 
