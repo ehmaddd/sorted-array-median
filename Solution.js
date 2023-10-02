24619d3fa4a7edb055097c37e7caa528a7c15a0b
ehmaddd <ehmaddd@gmail.com>
2023-10-02 12:49:58 +0500

    Add function

--- Solution.js
+++ Solution.js
@@ -1,7 +1,12 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+
+  const merged = nums1.concat(nums2);
+  const sorted = merged.sort(function(a, b){return a - b});
+  console.log(sorted);
+};
+
 const nums1 = [1,3];
 const nums2 = [2];
+findMedianSortedArrays(nums1, nums2);
 
-const merged = nums1.concat(nums2);
 
-const sorted = merged.sort(function(a, b){return a - b});
-console.log(sorted);
